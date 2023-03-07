709520c7208f10870b5d38a64cbe6c22c206fd72
mio2smug <bryan.lazarowork@yahoo.com>
2023-03-07 17:23:02 -0500

    Sidebar works now

--- src/sidebar.js
+++ src/sidebar.js
@@ -5,9 +5,10 @@ const addToSidebar = () =>{
     const sideBarSubmit = document.querySelector('#sidebar--submit--button')
     const sideBarInput = document.querySelector('#sidebar--input') 
     const sideBarSpan = document.querySelector('#my--sidebar--span')
+    let sidebarContent = document.querySelector('.sidebar--content--wrapper')
     
     //JS constants
-    let myInput;
+    // let myInput;
     let myDays = []
 
     function day(date){
@@ -15,6 +16,7 @@ const addToSidebar = () =>{
     }
     
     addButtonSidebar.onclick = function () {
+        clearModal();
         sidebarModal.style.display = 'block'
     };
 
@@ -25,9 +27,24 @@ const addToSidebar = () =>{
     sideBarSubmit.onclick = function(){
         addDayToLibrary();
         clearModal();
+        display();
         sidebarModal.style.display = 'none'
     };
 
+
+    function display (){
+        for (let i = 0; i < myDays.length; i++){
+            const myDiv = document.createElement('div')
+            myDiv.classList.add('sidebar--template')
+            const myP = document.createElement('p')
+            myP.textContent = myDays[i].date  
+            // console.log("hello?")
+            myDiv.appendChild(myP)
+            // console.log("hi?")
+            sidebarContent.appendChild(myDiv);
+        }
+    }
+    
     const addDayToLibrary = () => {myDays.push(new day(sideBarInput.value))}
     
     const clearModal = () => sideBarInput.value = ''
